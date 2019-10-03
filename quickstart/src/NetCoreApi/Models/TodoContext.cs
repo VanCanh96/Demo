6af51e016ad52369c1d7db157136713b3a248cfa
tam.n <tam.n@3si.com>
2019-10-03 10:58:07 +0700

    commit NetCoreApi

---
+++ quickstart/src/NetCoreApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
